e2f5b5876a135359bd80af9a29f639f05db4389c
Parvesh Rawal <INSHOP07@outlook.com>
2025-10-02 23:02:48 +0530

    Update dice_sim.py

--- Random Dice/dice_sim.py
+++ Random Dice/dice_sim.py
@@ -1,1 +1,71 @@
 
+import random
+#CATEGORIZING OUTCOME INTO A LIST
+
+one =   """ 
+            ("===========")
+            ("|         |")
+            ("|    O    |")
+            ("|         |")
+            ("===========")\n  
+        
+        """
+
+two =   """ 
+            ("===========")
+            ("|         |")
+            ("| O     O |")
+            ("|         |")
+            ("===========")\n  
+        
+        """
+
+
+
+three =   """ 
+            ("===========")
+            ("|    O    |")
+            ("|    O    |")
+            ("|    O    |")
+            ("===========")\n  
+        
+        """
+
+four =   """ 
+            ("===========")
+            ("|  O    O |")
+            ("|     0   |")
+            ("|  O    O |")
+            ("===========")\n  
+        
+        """
+
+five =   """ 
+            ("===========")
+            ("| O     O |")
+            ("|    0    |")
+            ("| O     O |")
+            ("===========")\n  
+        
+        """
+
+six =  """
+            ("===========") 
+            ("| O     O |")
+            ("| O     O |")
+            ("| O     O |")
+            ("===========") \n      
+        """
+
+outcomes = [one, two, three, four, five, six]
+
+print("=================================")
+
+x = "y"
+
+while x == "y":
+    randon_outcome = random.choice(outcomes)
+    for outcome in randon_outcome:
+        print(outcome)
+    
+    x =  input("Press y to roll again ")
